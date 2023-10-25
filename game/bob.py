e4789165ccf8b8af149d60b4926b1b3736ffd6ea
Mariame <mariame.oubani@insa-rouen.fr>
2023-10-25 13:21:05 +0200

    bob part1

--- game/bob.py
+++ game/bob.py
@@ -1,5 +1,6 @@
 # define visual Bob:
 import pygame as pg
+import random
 
 class Bob:
 
@@ -27,6 +28,36 @@ class Bob:
 
         
 
-    
+    #def __init__(self, initial_population=100, max_energy=200, food_spawn_rate=200, energy_per_food=100):
+    #   self.population = [self.create_bob(max_energy) for _ in range(initial_population)]
+    #    self.food_spawn_rate = food_spawn_rate
+    #    self.energy_per_food = energy_per_food
 
+    def create_bob(self, max_energy):
+        return {
+            "energy": random.randint(1, max_energy),
+            "velocity": random.uniform(0.9, 1.1),
+            "mass": random.uniform(0.9, 1.1),
+            "perception": random.randint(0, 5),
+            "memory": random.randint(0, 5),
+        }
+
+    def spawn_food(self):
+        food_energy = random.randint(1, self.energy_per_food)
+        return {
+            "energy": food_energy,
+            "x": random.randint(0, 100),
+            "y": random.randint(0, 100),
+        }
+
+    def simulate(self, num_days):
+        for day in range(num_days):
+            if day % self.food_spawn_rate == 0:
+                food = self.spawn_food()
+                # Add food to the world
+
+            #for bob in self.population:
+                # Implement Bob's behaviors here
+
+            # Remove uneaten food at the end of the day
 
