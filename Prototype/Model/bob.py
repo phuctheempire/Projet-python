5300d30a591057c3927471e6ece6f07d2e4ec9df
Xuan Phuc <101117469+phuctheempire@users.noreply.github.com>
2023-11-10 01:40:11 +0100

    a

--- Prototype/Model/bob.py
+++ Prototype/Model/bob.py
@@ -3,5 +3,14 @@
 class Bob ( ):
     def __init__(self, coordX, coordY):
         self.coordX = coordX
-        self.coordY = coordY
+        self.coordY = coordY  
+
+    def update(self, vectX, vectY):
+        self.coordX += vectX
+        self.coordY += vectY
+    
+    
         
+{ "1": bob; }
+
+bob = Bob( 1, 1 )
