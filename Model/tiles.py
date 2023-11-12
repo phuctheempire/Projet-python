0ab26a7daf7e38811de5aaf280531ccb4227c470
Xuan Phuc <101117469+phuctheempire@users.noreply.github.com>
2023-11-12 16:46:01 +0100

    a

---
+++ Model/tiles.py
@@ -0,0 +1,9 @@
+from typing import TYPE_CHECKING
+import pygame as pg
+
+from view.texture import Texture
+from GameControl.settings import GRID_LENGTH, TILE_SIZE
+
+if TYPE_CHECKING:
+    from Bob
+
