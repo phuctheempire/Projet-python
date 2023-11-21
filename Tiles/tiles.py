4fc9e23ed9117db71a1d50e94c0c55853f8972a4
Xuan Phuc <101117469+phuctheempire@users.noreply.github.com>
2023-11-21 17:09:46 +0100

    a

--- Tiles/tiles.py
+++ Tiles/tiles.py
@@ -52,7 +52,9 @@ class Tile:
     def addBob( self, bob: 'Bob'):
         self.listBob.append(bob)
         bob.CurrentTile = self
+        bob.NextTile = bob.getNextTile()
         print(bob.CurrentTile)
+        print(bob.getNextTile())
     
     def removeBob(self, bob: 'Bob'):
         self.listBob.remove(bob)
