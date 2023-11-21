0099bda6d268b462673624947dd58d0c905987f1
Xuan Phuc <101117469+phuctheempire@users.noreply.github.com>
2023-11-21 18:38:11 +0100

    a

--- Tiles/Bob/bob.py
+++ Tiles/Bob/bob.py
@@ -93,7 +93,7 @@ class Bob:
     def setNextTile(self):
         print("Setting next tile")
         print("x")
-        # print(self.CurrentTile.getNearbyTiles(0))
+        print(self.CurrentTile)
 
         #Temporary
         nearbyTiles = self.CurrentTile.getNearbyTiles(0)
